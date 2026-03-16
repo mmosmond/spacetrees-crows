8d266ef7948cb4bf16cfcf8ac562030cd9f6765d
Matthew Osmond <mm.osmond@utoronto.ca>
2026-03-16 12:02:52 -0400

    dolores setup

---
+++ dolores-startup.sh
@@ -0,0 +1,10 @@
+#git clone https://github.com/a-ignatieva/dolores.git
+#cd dolores/
+#module load python/3.11.4
+#python -m venv dolores-venv
+source dolores-venv/bin/activate
+#module load gcc/12.3
+#module load gsl/2.7
+#pip install -r requirements.txt
+#had to remove brackets from stored_nbytes() in a tscompress file to run the example
+#python -m run-dolores -C chr18 -n chr18 -t crows --genetic_map_loc crows/chr18.map -s None --genetic_map_stdpopsim None
